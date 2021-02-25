6c8b299e22fec41fa546cc3a7d2f74016c5f2351
Tyler van der Hoeven <hi@tyvdh.com>
2021-02-25 10:57:27 -0500

    Added support for txFunction upload fees

--- wrangler/src/txFunctions/upload.js
+++ wrangler/src/txFunctions/upload.js
@@ -1,13 +1,14 @@
 import { response } from 'cfw-easy-utils'
 import shajs from 'sha.js'
 import BigNumber from 'bignumber.js'
-import { Keypair } from 'stellar-base'
+import { Transaction, Keypair, Networks } from 'stellar-base'
+import { find } from 'lodash'
 
 export default async ({ request }) => {
   const body = await request.formData()
 
   const txFunctionFields = body.get('txFunctionFields')
-  const txFunctionFieldsBuffer = Buffer.from(txFunctionFields, 'base64')
+  const txFunctionFieldsBuffer = txFunctionFields ? Buffer.from(txFunctionFields, 'base64') : Buffer.from('')
 
   const txFunction = body.get('txFunction')
   const txFunctionBuffer = Buffer.from(txFunction)
@@ -24,6 +25,54 @@ export default async ({ request }) => {
   const txFunctionSignerPublicKey = txFunctionSignerKeypair.publicKey()
 
   const cost = new BigNumber(txFunctionBufferLength).dividedBy(1000).toFixed(7)
+
+  try {
+    const txFunctionFee = body.get('txFunctionFee')
+
+    const transaction = new Transaction(txFunctionFee, Networks[STELLAR_NETWORK])
+    const transactionHash = transaction.hash().toString('hex')
+
+    if (!find(transaction._operations, (op) => 
+      op.type === 'payment'
+      && op.destination === TURRET_ADDRESS
+      && new BigNumber(op.amount).isGreaterThanOrEqualTo(cost)
+      && op.asset.isNative()
+    )) throw 'Missing or invalid txFunctionFee'
+
+    await fetch(`https://horizon-testnet.stellar.org/transactions/${transactionHash}`)
+    .then((res) => {
+      if (res.ok)
+        throw 'txFunctionFee has already been submitted'
+      else if (res.status === 404)
+        return
+      else
+        throw res
+    })
+
+    const horizon = STELLAR_NETWORK === 'PUBLIC' ? 'https://horizon.stellar.org' : 'https://horizon-testnet.stellar.org'
+    const tx = transaction.toXDR()
+    const txBody = new FormData()
+          txBody.append('tx', tx)
+
+    await fetch(`${horizon}/transactions`, {
+      method: 'POST',
+      body: txBody
+    }).then((res) => {
+      if (res.ok)
+        return res.json()
+      throw res
+    })
+  }
+  
+  catch(err) {
+    return response.json({
+      message: typeof err === 'string' ? err : 'Failed to process txFunctionFee',
+      status: 402,
+      cost,
+    }, {
+      status: 402
+    })
+  }
 
   await TX_FUNCTIONS.put(txFunctionHash, Buffer.concat([txFunctionBuffer, txFunctionFieldsBuffer]), {
     metadata: {
