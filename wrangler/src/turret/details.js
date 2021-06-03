445a2223e4cce98f6170102bd87dd2db83367593
Tyler van der Hoeven <hi@tyvdh.com>
2021-06-03 09:28:03 -0400

    point to new CF account, add days to fees object in Turret details endpoint

--- wrangler/src/turret/details.js
+++ wrangler/src/turret/details.js
@@ -9,12 +9,13 @@ export default async () => {
     version: VERSION,
     fee: {
       min: XLM_FEE_MIN,
-      max: XLM_FEE_MAX
+      max: XLM_FEE_MAX,
+      days: TX_FUNCTION_FEE_DAYS_TTL
     },
     divisor: {
       upload: UPLOAD_DIVISOR, 
       run: RUN_DIVISOR
-    }
+    },
   }, {
     headers: {
       'Cache-Control': 'public, max-age=300', // 5 minutes
