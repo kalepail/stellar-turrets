8d3c71445a24f357d0c10872e4a5d769420b3229
Tyler van der Hoeven <hi@tyvdh.com>
2021-10-03 14:30:47 -0400

    Update txFunction-raw.js

--- wrangler/contracts/txFunction-raw.js
+++ wrangler/contracts/txFunction-raw.js
@@ -1,21 +1,17 @@
+const { TransactionBuilder, Server, Networks, BASE_FEE, Operation, Asset } = require('stellar-sdk')
+
+const server = new Server(HORIZON_URL)
+
 module.exports = (body) => {
-  const { TransactionBuilder, Networks, BASE_FEE, Operation, Asset, Account } = StellarBase
   const { source, destination } = body
 
-  return fetch(`https://horizon-testnet.stellar.org/accounts/${source}`)
-  .then((res) => {
-    if (res.ok)
-      return res.json()
-    throw res
-  })
-  .then((account) =>
-    new TransactionBuilder(
-      new Account(account.id, account.sequence), 
-      { 
-        fee: BASE_FEE, 
-        networkPassphrase: Networks.TESTNET
-      }
-    )
+  return server
+  .loadAccount(source)
+  .then((account) => {
+    return new TransactionBuilder(account, { 
+      fee: BASE_FEE, 
+      networkPassphrase: Networks[STELLAR_NETWORK],
+    })
     .addOperation(Operation.payment({
       destination,
       asset: Asset.native(),
@@ -24,5 +20,5 @@ module.exports = (body) => {
     .setTimeout(0)
     .build()
     .toXDR()
-  )
+  })
 }
